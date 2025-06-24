7df71eed7421093ce49dad044bb69fa384e1a564
TImoteo Grebosz <74367166+Escanor68@users.noreply.github.com>
2025-06-23 22:01:22 -0300

    error en las imagenes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,7 +5,7 @@ function Item({ product }) {
   return (
     <div className="item">
       <h3>{product.title}</h3>
-      <img src={product.image} alt={product.title} width={100} />
+      <img src={product.thumbnail} alt={product.title} width={100} />
       <p>Precio: ${product.price}</p>
       <Link to={`/item/${product.id}`}>Ver detalle</Link>
     </div>
